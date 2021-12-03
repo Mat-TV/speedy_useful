a8ec2b4798808fcde823047d9b4bd8543d3fe2ed
Mat Troncoso Villar <82126178+Trufumut@users.noreply.github.com>
2021-12-03 16:18:12 -0300

    Actualización Anomalias_Tropicales
    Agregados nuevos experimentos; ampliado el soporte a Windows

--- Anomalias_Tropicales.m
+++ Anomalias_Tropicales.m
@@ -3,24 +3,26 @@
 aseo % https://github.com/Trufumut/MATLAB_useful/blob/main/aseo.m
 %% Preliminares
 %ruta a los archivos
-% path1 = '/home/danveloso/Documentos/Cond_borde_SPEEDY_revision/';
-path1 = '/home/matt/speedy_ver41.5/data/bc/t30/'; % path de datos
-path2 = '/home/matt/Documentos/SPEEDY/'; % path de utensilios
-cd '/home/matt/Documentos/SPEEDY/' 
+cd 'C:\Users\fredo\Documents\Geof\Tópico 2021-2\Final\'
+% path1 = '/home/matt/speedy_ver41.5/data/bc/t30/'; % path de datos % LINUX
+% path2 = '/home/matt/Documentos/SPEEDY/'; % path de utensilios %LINUX
+path1 = 'C:\Users\fredo\Documents\Geof\Tópico 2021-2\Final\in\'; % WINDOWS
+path2 = 'C:\Users\fredo\Documents\Geof\Tópico 2021-2\Final\out\'; % WINDOWS
+% cd '/home/matt/Documentos/SPEEDY/'  % LINUX
 %----Climatology that comes with Speedy package
 
-% fid = fopen([path1,'sst_clim6110Hadisst.t30.sea.grd'], 'r')
-[fid,mensaje] = fopen([path1, 'clim/sst_clim6110Hadisst.t30.sea.grd'], 'r');
+% [fid,mensaje] = fopen([path1, 'clim/sst_clim6110Hadisst.t30.sea.grd'], 'r'); % LINUX
+[fid,mensaje] = fopen([path1, 'sst_clim6110Hadisst.t30.sea.grd'], 'r'); % WINDOWS
 variable = fread(fid, 'float','b'); % archivo de dim 55320x1
 
 sstc=reshape(variable,4610,12); %4610*12=55320
 sstc=sstc(2:48*96+1,:); %48*96=4608 --> valor 1 y end es 2.5829e-41 (distinto a -9.999e+19 = NaN)
 sstc=reshape(sstc,96,48,12);
 
-sstc=flipdim(sstc,2); % --> averiguar si parte desde +90° o desde -90°--> Parece que desde -90°
+sstc=flipdim(sstc,2); % --> averiguar si parte desde +90Â° o desde -90Â°--> Parece que desde -90Â°
 sstc=permute(sstc,[3 2 1 ]); %%%--------> IMPORTANTE! Dimensiones = (tiempo,latitud,longitud)
 
-mm=min(min(min(sstc))); %-9.999e+19 = NaN (el valor + pequeño)
+mm=min(min(min(sstc))); %-9.999e+19 = NaN (el valor + pequeÃ±o)
 ii=find(sstc==mm); % ii contiene posiciones de los valores NAN,
 SSTC = sstc; % para climatologia sin NaN
 sstc(ii)=NaN;
@@ -46,7 +48,7 @@ sstc(ii)=NaN;
 % ssta=flipdim(ssta,2);
 % ssta=permute(ssta,[3 2 1 ]);
 
-%%% IMPORTANTE: Resolución y puntos de grilla usados en SPEEDY
+%%% IMPORTANTE: ResoluciÃ³n y puntos de grilla usados en SPEEDY
 X=0:3.75:360; X=X(1:96);
 Y=[-87.159   -83.479   -79.777   -76.070   -72.362   -68.652 -64.942   -61.232  -57.521   -53.810   -50.099   -46.389   -42.678   -38.967 -35.256   -31.545   -27.833  -24.122   -20.411   -16.700   -12.989    -9.278 -5.567    -1.856     1.856     5.567    9.278    12.989    16.700    20.411  24.122    27.833    31.545    35.256    38.967  42.678    46.389    50.099 53.810    57.521    61.232    64.942    68.652    72.362 76.070    79.777 83.479    87.159];
 
@@ -58,11 +60,14 @@ Y=[-87.159   -83.479   -79.777   -76.070   -72.362   -68.652 -64.942   -61.232  
 %----HadISST SST 1870/01-2017/04, dim=360x180x1768
 %----HadISST SST 1870/01-2021/07, dim=360x180x1819 % ESTE ES TÓPICO 2021
 
-SST=ncread([path2,'HadISST_sst.nc'],'sst');
+% modelo = [path2,'HadISST_sst.nc']; % LINUX
+modelo = 'HadISST_sst.nc'; % WINDOWS
+
+SST=ncread(modelo,'sst');
 %SST2=ncread('/home/marcelo/data/ERSSTv.2_Jan2007.cdf','SST');
-lon=ncread([path2,'HadISST_sst.nc'],'longitude'); % -180:+180
-lat=ncread([path2,'HadISST_sst.nc'],'latitude'); % +89:-89
-fechasSST=double(ncread([path2,'HadISST_sst.nc'],'time'));
+lon=ncread(modelo,'longitude'); % -180:+180
+lat=ncread(modelo,'latitude'); % +89:-89
+fechasSST=double(ncread(modelo,'time'));
 fecha0 = datenum('01-01-1870');
 fechasSST = fechasSST + fecha0; % enero 1870:julio 2021
 % ncdisp('HadISST_sst.nc')
@@ -132,7 +137,7 @@ Lon = single([0 0.5:1:359.5])';
 [SST2]=interpol2(Lon,flipdim(lat,1),flipdim(SST2,2),X,Y); %--> hacer flipdim SST y lat
 clear id1000 ii Lon SSTclim
 % % -----------------------------------------------------
-% % Esto es para no hacer todos los cálculos de nuevo hasta acá:
+% % Esto es para no hacer todos los cÃ¡lculos de nuevo hasta acÃ¡:
 % %  save('calculados')
 %  calculados=load('calculados')
 %  nombres=structvars(calculados); % fx adjunta
@@ -147,80 +152,73 @@ for j=1:12
   m=j;
   while (m<=length(fechasSST))
     SSTa(m,:,:)=SST2(m,:,:)-clim(j,:,:);
+    SSTa(m,:,:)=0;
     m=m+12;
   end
 end
 %
 % De aquí, solo correr el experimento deseado
 % % % % % ----------------
-% Experimento 1: toda la banda ecuatorial
-% exp=1;
-% indxeq = find(Y>-13&Y<13); 
-% for j=1:12
-%   m=j;
-%   while (m<=length(fechasSST))
-%     SSTa(m,indxeq,:)=1;%clim(j,indxeq,:);
-%     m=m+12;
-%   end
-% end
+% Experimento 1: solo climatología
+exp=1;
 %
 % % % % % ----------------
 % Experimento 2: zona el niño
-exp=2;
-indxeq = find(Y>-6 & Y<6);
-indyeq = find(X>190 & X<=240);
-for j=1:12
-  m=j;
-  while (m<=length(fechasSST))
-    SSTa(m,indxeq,indyeq)=1; % aquí le pongo la alteración (-1 y +1)
-    m=m+12;
-  end
-end
+% exp=2;
+% indxeq = find(Y>-6 & Y<6);
+% indyeq = find(X>190 & X<=240);
+% for j=1:12
+%   m=j;
+%   while (m<=length(fechasSST))
+%     SSTa(m,indxeq,indyeq)=-1; % aquÃ­ le pongo la alteraciÃ³n (-1 y +1)
+%     m=m+12;
+%   end
+% end
 % % % % % ----------------
 %
 %Set to 0 anomalies over continents
-for t=1:length(fechasSST)
-    ll = find(SSTC(1,:,:) == mm);
-    SSTa(t,ll)=0; % AQUÍ HABÍA UN 0 PERO PONER NaN para visualizar
-end
-
-clim=clim+273.15; %aquí lo deja en Kelvin
+% for t=1:length(fechasSST)
+%     ll = find(SSTC(1,:,:) == mm);
+%     SSTa(t,ll)=0; % AQUÃ? HABÃ?A UN 0 PERO PONER NaN para visualizar
+% end
+% 
+clim=clim+273.15; %aquÃ­ lo deja en Kelvin
 
 % break
 % clim_nonan=clim
 % clim_nonan(clim_nonan<-100) == NaN
 %
 %% Visualizar los elementos a guardar
-% Ver la climatología
+% Ver la climatologÃ­a
 figure
 for i=1:12
     contourf(X',Y,squeeze(clim_NaN(i,:,:))), colorbar,caxis([-5 30])
     title(num2str(i)) 
     pause(0.5)
 end
-% Ver la anomalía, al menos para algunas fechas puestas a mano
+% Ver la anomalÃ­a, al menos para algunas fechas puestas a mano
 figure
 contourf(X',Y,squeeze(SSTa(500,:,:))), colorbar
 %
 %% Guardar los datos
 %------ Save New Clim 
 
-clim2=flipdim(clim,2);   % voltea la dimensión Y
+clim2=flipdim(clim,2);   % voltea la dimensiÃ³n Y
 clim2=permute(clim2,[3 2 1]);
 clim2=reshape(clim2,[length(clim2(:,1,1))*length(clim2(1,:,1)),12]); % X*Y=4608
 dummy=ones(4610,12)*variable(1);   % crea una matriz de 2.5829e-41
 dummy(2:48*96+1,:)=clim2;          % rellena los espacios correspondientes
 dummy=dummy(:); % hace de dummy una sola columna
 
-% fidw = fopen([path1,'Cond_borde/sst_clim6110Hadisst.t30.sea.grd'],'w')
-fidw = fopen([path1, char('clim/sst_cEXP' + string(exp) + '.t30.sea.grd')], 'w');
+% fidw = fopen([path1, char('clim/sst_cEXP' + string(exp) + '.t30.sea.grd')], 'w'); % LINUX
+fidw = fopen([path2, char('sst_cEXP' + string(exp) + '.t30.sea.grd')], 'w'); % WINDOWS
 count = fwrite(fidw,dummy,'float','b');
 %
 clear dummy
 
 %------ Save New Anomalies
 
-SSTa2=flipdim(SSTa,2);   % voltea la dimensión Y
+SSTa2=flipdim(SSTa,2);   % voltea la dimensiÃ³n Y
 % SSTa2=permute(SSTa2,[1 3 2]);
 SSTa2=permute(SSTa2,[3 2 1]);
 % SSTa2=SSTa(:,:)';
@@ -229,8 +227,8 @@ dummy=ones(4610,length(fechasSST))*variable(1);   % crea una matriz de 2.5829e-4
 dummy(2:48*96+1,:)=SSTa2;            % rellena los espacios correspondientes
 dummy=dummy(:);
 
-% fidw =fopen([path1,'Cond_borde/sst_anom6110Hadisst.t30.sea.grd'],'w')
-fidw = fopen([path1, char('anom/sst_aEXP' + string(exp) + '.t30.grd')], 'w');
+% fidw = fopen([path1, char('anom/sst_aEXP' + string(exp) + '.t30.grd')], 'w'); % LINUX
+fidw = fopen([path2, char('sst_aEXP' + string(exp) + '.t30.grd')], 'w'); % WINDOWS
 count = fwrite(fidw,dummy,'float','b');
 %
 clear dummy
